a260643808144adb618eb580994ba91cb06d1287
AdvancedEel850 <90942089+AdvancedEel850@users.noreply.github.com>
2022-09-29 13:39:17 -0600

    Add new menu, and file capability

--- Calculator.cpp
+++ Calculator.cpp
@@ -2,17 +2,57 @@
 #include <fstream>
 using namespace std;
 
-int file(double result, double a, double b, string mathfunc)
+int file_history()
 {
     fstream my_file;
-    my_file.open("my_file", ios::out);
+    string line;
+
+    cout << endl;
+
+    my_file.open("history.txt", ios::in);
     if (!my_file)
     {
         cout << "File not Created.";
     }
     else
     {
-        my_file << a << " " + mathfunc + " " << b << " = " << result;
+        char info;
+        while(getline(my_file,line))
+        {
+            cout << line << endl;
+        }
+        my_file.close();
+    }
+    cout << endl;
+    return 0;
+}
+int file_clear()
+{
+    fstream my_file;
+    my_file.open("history.txt", ios::out);
+    if (!my_file)
+    {
+        cout << "File not Created.";
+    }
+    else
+    {
+        my_file << "";
+        my_file.close();
+    }
+    return 0;
+
+}
+int file_write(double result, double a, double b, string mathfunc)
+{
+    fstream my_file;
+    my_file.open("history.txt", ios::app);
+    if (!my_file)
+    {
+        cout << "File not Created.";
+    }
+    else
+    {
+        my_file << a << " " + mathfunc + " " << b << " = " << result << endl;
         my_file.close();
     }
     return 0;
@@ -25,7 +65,7 @@ int addition(double a, double b)
 
     cout << "Result is: " << result << endl << endl;
 
-    file(result, a, b, "addition");
+    file_write(result, a, b, "+");
     return 0;
 }
 
@@ -37,6 +77,7 @@ int subtraction(double a, double b)
 
     cout << "Result is: " << result << endl << endl;
 
+    file_write(result, a, b, "-");
     return 0;
 }
 
@@ -48,6 +89,7 @@ int multiplication(double a, double b)
 
     cout << "Result is: " << result << endl << endl;
 
+    file_write(result, a, b, "X");
     return 0;
 }
 
@@ -58,21 +100,22 @@ int division(double a, double b)
 
     result = a / b;
 
-    cout << "Result is:" << result << endl << endl;
+    cout << "Result is: " << result << endl << endl;
 
+    file_write(result, a, b, "/");
     return 0;
 }
 
 int menu()
 {
     double count;
-    while(count != 5)
+    while(count != 0)
     {
         cout << "1) Add" << endl;
         cout << "2) Subtract" << endl;
         cout << "3) Multiply" << endl;
         cout << "4) Divide" << endl;
-        cout << "5) Quit" << endl << endl;
+        cout << "0) Quit" << endl << endl;
         cin >> count;
 
         if(count == 1)
@@ -118,5 +161,28 @@ int menu()
 
 int main()
 {
-    menu();
+    double count;
+    while(count != 0)
+    {
+        cout << "1) Mathmatical functions" << endl;
+        cout << "2) See history" << endl;
+        cout << "3) Clear history" << endl;
+        cout << "0) quit" << endl << endl;
+
+        cin >> count;
+
+        if(count == 1)
+        {
+            menu();
+
+        }
+        if(count == 2)
+        {
+            file_history();
+        }
+        if(count == 3)
+        {
+            file_clear();
+        }
+    }
 }
